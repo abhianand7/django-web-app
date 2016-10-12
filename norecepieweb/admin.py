c54d8c8ffe8185a9214fc66c94176d2b4eda09a5
abhinav <erabhinav@outlook.com>
2016-10-12 22:30:38 +0530

    admin and model updates

--- norecepieweb/admin.py
+++ norecepieweb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from cms.extensions import PageExtensionAdmin
+
+from .models import IconExtension
+
+
+class IconExtensionAdmin(PageExtensionAdmin):
+    pass
+
+admin.site.register(IconExtension, IconExtensionAdmin)
--- norecepieweb/models.py
+++ norecepieweb/models.py
@@ -2,4 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from cms.extensions import PageExtension
+from cms.extensions.extension_pool import extension_pool
+
+
+class IconExtension(PageExtension):
+    image = models.ImageField(upload_to='icons')
+
+extension_pool.register(IconExtension)
+
