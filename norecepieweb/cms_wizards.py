d7230b05431ea2df26e04d4f9b3f971d13f48703
abhinav <erabhinav@outlook.com>
2016-10-14 18:31:34 +0530

    updates

---
+++ norecepieweb/cms_wizards.py
@@ -0,0 +1,18 @@
+# from cms.wizards.wizard_base import Wizard
+# from cms.wizards.wizard_pool import wizard_pool
+#
+# from norecepieweb.forms import FutureModelForm
+# from norecepieweb.models import FutureModel
+#
+#
+# class CreateWizard(Wizard):
+#     pass
+#
+# wizard = CreateWizard(
+#     title="Poll",
+#     weight=200,  # determines the ordering of wizards in the Create dialog
+#     form=FutureModelForm,
+#     description="Create new Model",
+# )
+#
+# wizard_pool.register(wizard)
--- norecepieweb/models.py
+++ norecepieweb/models.py
@@ -12,7 +12,10 @@ class IconExtension(PageExtension):
 
 class TitleExtensions(PageExtension):
     title = models.TextField()
-
+# create a model and register it and then import it into cms_wizard.py and forms.py to make a wizard so that
+# it can be created directly from the frontend
+# class FutureModel():
+#     pass
 
 extension_pool.register(IconExtension)
 extension_pool.register(TitleExtensions)
